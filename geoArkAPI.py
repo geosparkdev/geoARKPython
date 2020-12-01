93593bd54edfff02a2eddd5c3a9c71d7666e9231
Tiffany Young <tcy8v6@umsystem.edu>
2020-11-30 20:20:13 -0600

    push

--- geoArkAPI.py
+++ geoArkAPI.py
@@ -770,8 +770,8 @@ def create_app(test_config=None):
         totals_sorted=totals.sort_values('total_risk', ascending=False)
         totals_sorted=totals_sorted.astype(str)
 
-        counties_list=list(totals_sorted['County Name'].unique())
-        totals_list=list(totals_sorted.total_risk.unique())
+        counties_list=list(totals_sorted['County Name'].values)
+        totals_list=list(totals_sorted.total_risk.values)
 
         metadata=[{"factor":"total_risk","max":str(totals.total_risk.max())}]
 
