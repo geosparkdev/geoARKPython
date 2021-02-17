50ead2b1128fdb36e90218fc9df9dcba5814b0ec
tiffany <GeoSPARKdev@gmail.com>
2021-02-16 19:04:09 -0600

    push

--- geoArkAPI.py
+++ geoArkAPI.py
@@ -810,7 +810,6 @@ def create_app(test_config=None):
         db = client.covid_dash
         risk_factor=json.loads(request.data)
         datasources= pd.DataFrame(list(db.covid_sources.find({'risk_factor':risk_factor},{'_id':0,'source_description':0,'risk_factor':0})))
-        final=datasources.to_dict("records")
         return jsonify(final)
 
 
