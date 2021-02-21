9177e37d34bd177f63c7bcb7bf1058163e7a6809
tiffany <GeoSPARKdev@gmail.com>
2021-02-20 23:20:48 -0600

    push

--- geoArkAPI.py
+++ geoArkAPI.py
@@ -785,8 +785,8 @@ def create_app(test_config=None):
         ## accessibility, exposure, health resources, socioeconomic, susceptiblity, transmission
         db = client.covid_dash
         print('STUFF HERER HERHEHREHR')
-        print(request.data)
-        print(request.data[1])
+        print(request.data.risk_factors)
+     
 
         risk_factors_bool=json.loads(request.data[0])
         filter1_on=json.loads(request.data[1])
