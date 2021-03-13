db8bc6b5c6dc93664417b096c4008a67d4473c49
tiffany <GeoSPARKdev@gmail.com>
2021-03-12 20:33:58 -0600

    push

--- geoArkAPI.py
+++ geoArkAPI.py
@@ -800,13 +800,6 @@ def create_app(test_config=None):
         return jsonify([totals.to_dict('records'), metadata])
 
 
-    @app.route('/FilterUpdate', methods=['POST'])
-    def FilterUpdate():
-        tot_table=json.loads(request.data)
-        print(tot_table)
-
-        return 'okay'
-
 
     @app.route('/getTotalsFilter', methods=['POST'])
     def getTotalsFilter():
