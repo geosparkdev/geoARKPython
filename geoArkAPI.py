0eba48dcbf5f361c2b981be6c158178a47a5508c
tiffany <GeoSPARKdev@gmail.com>
2021-03-29 23:50:16 -0500

    factorsMapData quantiles

--- geoArkAPI.py
+++ geoArkAPI.py
@@ -588,7 +588,7 @@ def create_app(test_config=None):
         
 
         final=risk_factors[Q5_list]
-        final=final.rename(columns={'total':'total_Q5'})
+        final['total_Q5']=pd.qcut(final['total'], 5, labels=np.arange(5, 0, -1))
         final=final.astype(str)
 
         print(final)
