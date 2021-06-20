7c50b9ce66b01ae1f849c0af10208e9cc9a63201
tiffany <GeoSPARKdev@gmail.com>
2021-06-20 00:02:56 -0500

    push

--- geoArkAPI.py
+++ geoArkAPI.py
@@ -1152,8 +1152,8 @@ def create_app(test_config=None):
 
         if selected_data.attribute.nunique()>2:
             bin_labels_5 = [1, 2, 3, 4, 5]
-            selected_data['quants'] = pd.qcut(selected_data['attribute'],
-                                q=[0, .2, .4, .6, .8, 1],
+            selected_data['quants'] = pd.cut(selected_data['attribute'],
+                                bins=5,
                                 labels=bin_labels_5)
 
 
